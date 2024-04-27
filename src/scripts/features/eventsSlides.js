c33af525227727ec35764e85dbd7eeb9742328e5
Agathon006 <agaton615@gmail.com>
2024-04-27 13:27:53 +0300

    finished accordeon logic

--- src/scripts/features/eventsSlides.js
+++ src/scripts/features/eventsSlides.js
@@ -2,22 +2,26 @@ export default () => {
     const accordionItems = document.querySelectorAll('.accordion__item');
 
     accordionItems.forEach((item) => {
-      const header = item.querySelector('.accordion__item-header');
-      const body = item.querySelector('.accordion__item-body-wrapper');
-    
-      header.addEventListener('click', () => {
-        // Get the currently active accordion item body
-        const activeBody = document.querySelector('.accordion__item-body-wrapper.active');
-    
-        // If there is an active accordion item body, animate it out
-        if (activeBody) {
-          activeBody.classList.remove('active');
-          activeBody.classList.add('inactive');
-        }
-    
-        // Animate the current accordion item body in
-        body.classList.add('active');
-        body.classList.remove('inactive');
-      });
+        const header = item.querySelector('.accordion__item-header-wrapper');
+        const body = item.querySelector('.accordion__item-body-wrapper');
+
+        header.addEventListener('click', () => {
+            const activeHeader = document.querySelector('.accordion__item-header-background.accordion__item-header-background--monocolor');
+            const activeBody = document.querySelector('.accordion__item-body-wrapper.active');
+
+            if (activeHeader) {
+                activeHeader.classList.remove('accordion__item-header-background--monocolor');
+            }
+
+            header.children[0].children[0].classList.add('accordion__item-header-background--monocolor');
+
+            if (activeBody) {
+                activeBody.classList.remove('active');
+                activeBody.classList.add('inactive');
+            }
+
+            body.classList.add('active');
+            body.classList.remove('inactive');
+        });
     });
 }
